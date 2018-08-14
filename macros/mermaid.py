7a0c17f95098eb008e297cf42b1e57ea638fe30d
despiegk <kristof@incubaid.com>
2018-08-14 09:43:55 +0200

    refactored the errorhandler

--- macros/mermaid.py
+++ macros/mermaid.py
@@ -1,6 +1,6 @@
 
 def mermaid(doc, name, content, width=1024):
     
-    #TODO: *1 needs to be redone to use mermaid pure in javascript
+    #TODO: *4 needs to be redone to use mermaid pure in javascript
 
-    return "TODO: *1 mermaid macro not implemented yet"
+    return "TODO: *4 mermaid macro not implemented yet"
