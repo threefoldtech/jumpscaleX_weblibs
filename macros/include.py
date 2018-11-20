cda56a105cd8c3d6ee34d915fea6b57b6eef92da
kristof de spiegeleer <despiegk@kristofs-MacBook-Pro.local>
2018-11-20 14:19:36 +0100

    digitalmne

--- macros/include.py
+++ macros/include.py
@@ -1,12 +1,12 @@
 from Jumpscale import j
-def _file_process(content,start="",end="",part=""):
+def _file_process(content,start="",end="",start_include=True,end_include=True):
     content = j.core.text.strip(content)
-    if start=="" and end=="" and part=="":
+    if start=="" and end=="":
         return content
-    if start!="" and part!="":
-        raise RuntimeError("start and part cannot be defined in include at same time")
-    if part!="":
-        start = part
+    # if start!="" and part!="":
+    #     raise RuntimeError("start and part cannot be defined in include at same time")
+    # if part!="":
+    #     start = part
     start = start.lower()
     out=""
     state="START"
@@ -17,26 +17,48 @@ def _file_process(content,start="",end="",part=""):
             state = "FOUND"
             # prefix_space_length = len(line)-len(lstrip)  #is nr of spaces when starting the search
             # prefix_space = " "*prefix_space_length
-            out+="%s\n"%line
+            if start_include:
+                out+="%s\n"%line
             continue
         if state=="FOUND":
-            if part != "":
-                #means looking for part
-                if len(line)>0 and line[0]!=" ":
-                    return out
-            out+="%s\n"%line
+            # if part != "":
+            #     #means looking for part
+            #     if len(line)>0 and line[0]!=" ":
+            #         return out
             if end!="" and lstrip.find(end.lower())!=-1:
+                if end_include:
+                    out+="%s\n"%line
                 return out
+            out+="%s\n"%line
+
     return out
 
 
-def include(doc, name="test5",docsite="",repo="",branch="",start="",end="",part="", **args):
+def include(doc, name="test5",docsite="",giturl="",start="",end="",start_include=True,end_include=True, **args):
+    """
+
+    :param doc: is given by macro system, not to be used as macro argument
+    :param name: name of the file (if no extension then is markdown doc which ends with .md),
+            will look inside the docsite or giturl
+            e.g. Package.py which would be name inside the giturl as specified below
+    :param docsite: name of the docsite which has the info
+    :param giturl:  if not docsite used, can specify the giturl by means of url like
+            https://raw.githubusercontent.com/threefoldtech/digital_me/development_960/DigitalMeLib/servers/digitalme/
+    :param start: if specified then will look for this text and start the output from there
+    :param start_include: True, std the found start is included in the output
+    :param end:
+    :param end_include: True, std the found end is included in the output
+    :param args: given to text as jinja arguments (not implemented yet)
+    :return:
+    """
     name = name.lower()
 
+    if args != {}:
+        raise RuntimeError("arguments support for macro's not implemented yet")
 
-    if repo!="":
-        path=j.clients.git.getContentPathFromURLorPath(repo)
-        key = j.data.hash.md5_string("macro_include_%s_%s"%(repo,name))
+    if giturl!="":
+        path=j.clients.git.getContentPathFromURLorPath(giturl)
+        key = j.data.hash.md5_string("macro_include_%s_%s"%(giturl,name))
 
         def do(path="",name="",start="",end="",part=""):
             ext = j.sal.fs.getFileExtension(name)
@@ -53,9 +75,9 @@ def include(doc, name="test5",docsite="",repo="",branch="",start="",end="",part=
             if len(res)>1:
                 raise RuntimeError("found more than 1 document for:%s %s"%(doc,name))
             if len(res)==0:
-                raise RuntimeError("could not find document in repo:%s name:%s"%(repo,name))
+                raise RuntimeError("could not find document in giturl:%s name:%s"%(giturl,name))
             content = j.sal.fs.fileGetContents(res[0])
-            content2=_file_process(content=content,start=start,end=end,part=part)
+            content2=_file_process(content=content,start=start,end=end,start_include=start_include,end_include=end_include)
 
             if extlower in ["","md"]:
                 return content2
