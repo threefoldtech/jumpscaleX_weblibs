c8f6bd3720f91454136dbb5c5c076a5f9846b5cd
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2019-02-20 15:59:23 +0200

    Add gedis javascript client to web libs

---
+++ static/gedis/gedis_client.js
@@ -0,0 +1,39 @@
+const GEDIS_CLIENT = (function(){
+    console.log(SERVER);
+    var socket = new WebSocket(SERVER);
+    let connected = false;
+    let connect = ()=> {
+        return new Promise(res =>{
+            if(!connected){
+                socket.onopen = () => {
+                    connected = true
+                    res(true)
+                }
+            } else {
+                res(true)
+            }
+        })
+    }
+    let execute = (namespace, actor, command, args, headers) => {
+        return connect().then((res) => {
+            return new Promise((resolve, fail) => {
+                let message = {
+                    "command": `${namespace}.${actor}.${command}`,
+                    "args": args,
+                    "headers": headers,
+                }
+                socket.send(JSON.stringify(message))
+                socket.onmessage = function(e) {
+                    resolve(e.data)
+                }
+            })
+        })
+    }
+
+    let client = {};
+    client.execute = async (info) => {
+        return await execute(info['namespace'], info['actor'], info['command'],
+                             info['args'], info['headers'])
+    };
+    return client
+})()
