3b79aa54c59282d86c36f8afcfdcfd56721802ee
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-03-30 17:24:30 +0200

    update docsify.js

--- static/docsify/get.sh
+++ static/docsify/get.sh
@@ -1,18 +1,18 @@
 
 set -ex
 
-# wget -N unpkg.com/docsify/lib/docsify.min.js
+wget -N unpkg.com/docsify/lib/docsify.min.js
 # wget -N unpkg.com/docsify/lib/plugins/search.min.js
-# wget -N unpkg.com/prismjs/components/prism-bash.min.js
-# wget -N unpkg.com/prismjs/components/prism-python.min.js
-# wget -N cdn.jsdelivr.net/npm/echarts@latest/dist/echarts.min.js
-# wget -N cdn.jsdelivr.net/npm/docsify-echarts-plugin/lib/index.min.js
+wget -N unpkg.com/prismjs/components/prism-bash.min.js
+wget -N unpkg.com/prismjs/components/prism-python.min.js
+wget -N cdn.jsdelivr.net/npm/echarts@latest/dist/echarts.min.js
+wget -N cdn.jsdelivr.net/npm/docsify-echarts-plugin/lib/index.min.js
 # wget -N unpkg.com/docsify-plantuml/dist/docsify-plantuml.min.js
 # wget -N unpkg.com/docsify/lib/plugins/disqus.min.js
-# wget -N unpkg.com/docsify-themeable
+wget -N unpkg.com/docsify-themeable
 # wget -N cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js
-# wget -N unpkg.com/docsify-themeable/dist/css/theme-simple.css
-# wget -N unpkg.com/docsify/lib/themes/vue.css
+wget -N unpkg.com/docsify-themeable/dist/css/theme-simple.css
+wget -N unpkg.com/docsify/lib/themes/vue.css
 # wget -N cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.css
 # wget -N https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css -O bootstrap4.1.3.min.css
 # wget -N https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js -O bootstrap4.1.3.min.js
@@ -37,9 +37,9 @@ set -ex
 # wget -N https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js
 
 
-git clone https://github.com/kjdion84/bootadmin 
-mv bootadmin/{js,css,webfonts}/* .
-rm -rf bootadmin
+#git clone https://github.com/kjdion84/bootadmin 
+#mv bootadmin/{js,css,webfonts}/* .
+#rm -rf bootadmin
 # #bootadmin
 # wget -N https://raw.githubusercontent.com/kjdion84/bootadmin/master/js/moment.min.js
 # wget -N https://raw.githubusercontent.com/kjdion84/bootadmin/master/js/bootadmin.min.js
